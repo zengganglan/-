328e9668e5268b1c38e185aa713588272f9912e2
zgl <1421026980@qq.com>
2018-12-28 00:09:54 +0800

    心理资讯项目第一次提交

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+import App from './App'
+Vue.use(ElementUI);
+import router from './router'
+//引入公共样式文件
+import "./assets/css/common.css"
+
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/views/layout/layout.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="app-wrapper">
+        <div class="head">
+            <top/>
+        </div>
+        <div class="main-container">
+            <Navbar/>
+         <transition name="fade" mode="out-in">
+            <router-view>1</router-view>
+        </transition>
+        </div>
+    </div>
+</template>
+
+<script>
+    import top from "./head"
+    import Navbar from "./Navbar"
+   
+    export default {
+        name:'layout',
+        components:{
+            top,
+            Navbar
+        }
+    }
+</script>
+
+<style scoped>
+.app-wrapper{
+    padding-top: 40px;
+
+}
+  .head{
+      position: fixed;
+      width: 100%;
+      top: 0;
+      left: 0;
+      height: 40px;
+      background-color: aquamarine;
+  }
+  .main-container{
+/* 由两个子元素撑开 */
+  display: flex;
+  }
+  .main-container>div:nth-child(2){
+      flex: 1;
+      background-color: azure;
+  }
+</style>
