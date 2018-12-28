229aff90adf8e385fc94be4364a5a6da67320b29
zgl <zengganglan@163.com>
2018-12-28 11:20:31 +0800

    心理咨询

---
+++ src/views/layout/head.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="a1">
+        
+        <div class="h-left">
+       中学心理管理系统
+        </div>
+        <div class="h-right">
+            <ul>
+                <li>首页</li>
+                <li>提醒</li>
+                <li>帮助</li>
+                <li>你好<span>XXX</span></li>
+                <li>退出</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.a1{
+    height: 40px;
+    // width: 100%;
+    background-color: #FCE7E7;
+}
+   .h-left{
+       float: left;
+       height: 40px;
+       line-height: 40px;
+   }
+    .h-right{
+       float: right;
+       height: 40px;
+
+       >ul{
+           float: right;
+           height: 40px;
+           line-height: 40px;
+
+           >li{
+               float: left;
+               margin-right: 20px;
+           }
+       }
+   }
+   
+</style>
---
+++ src/views/layout/school.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="scrren">
+     <!-- 初始页面 -->
+     <div class="box">
+       <div class="box1">box1</div>
+       <div class="box2">box2</div>
+
+     </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
